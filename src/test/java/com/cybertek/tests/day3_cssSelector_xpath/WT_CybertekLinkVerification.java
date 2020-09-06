06b707d1905fb314d25049ad32ea6c269d1707fc
minara35 <minara35@gmail.com>
2020-09-06 15:27:13 -0400

    add day3

--- src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
@@ -1,9 +1,9 @@
 package com.cybertek.tests.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cybertek.untilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 /*
 TC #3: PracticeCybertek/ForgotPassword URL verification
@@ -16,37 +16,65 @@ TC #3: PracticeCybertek/ForgotPassword URL verification
  */
 public class WT_CybertekLinkVerification {
     public static void main(String[] args) {
+        //TC #3: PracticeCybertek/ForgotPassword URL verification
+        //1. Open Chrome browser
+       // WebDriverManager.chromedriver().setup();
+       // WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        //2.Go to http://practice.cybertekschool.com/forgot_password
+        //2. Go to http://practice.cybertekschool.com/forgot_password
+
         driver.get("http://practice.cybertekschool.com/forgot_password");
 
-        //3.Enter any email into input box
-        driver.findElement(By.name("email")).sendKeys("minara@domain.com");
-       // 4.Click on Retrieve password
-        driver.findElement(By.id("form_submit")).click();
-        //5.Verify URL contains: Expected: “email_sent”
+        //3. Enter any email into input box
+        //driver.findElement(By.name("email")).sendKeys("anyemail@domain.com");
+
+        WebElement emailInputBox = driver.findElement(By.name("email"));
+
+        emailInputBox.sendKeys("anyemail@domain.com");
+
+        //4. Click on Retrieve password
+
+        //driver.findElement(By.id("form_submit")).click();
+
+        WebElement retrievePasswordButton = driver.findElement(By.id("form_submit"));
+        retrievePasswordButton.click();
+
+        //5. Verify URL contains:
+        //Expected: “email_sent”
         String expectedInUrl = "email_sent";
         String actualUrl = driver.getCurrentUrl();
-        if(actualUrl.contains(expectedInUrl)){
-            System.out.println("URL verification PASSED");
-        }else {
-            System.out.println("URL verification FAILED");
-        }
-//Verify textbox displayed the content as expected.Expected: “Your e-mail’s been sent!”
-        String actualText = driver.findElement(By.name("confirmation_message")).getText();
 
+        if (actualUrl.contains(expectedInUrl)){
+            System.out.println("URL verification PASSED!");
+        }else {
+            System.out.println("URL verification FAILED!!!");
+        }
+
+        //6. Verify textbox displayed the content as expected.
+        //Expected: “Your e-mail’s been sent!”
+
+        //verify the text
+        WebElement confirmationMessage = driver.findElement(By.name("confirmation_message"));
+
+        String actualText = confirmationMessage.getText();
         String expectedText = "Your e-mail's been sent!";
+
         System.out.println("actualText = " + actualText);
         System.out.println("expectedText = " + expectedText);
-        if(actualText.equalsIgnoreCase(expectedText)){
-            System.out.println("Header text value verification PASSED");
-        }else {
-            System.out.println("Header text value verification FAILED");
 
+        if (actualText.equals(expectedText)){
+            System.out.println("Header text value verification PASSED!");
+        }else{
+            System.out.println("Header text value verification FAILED!!!");
         }
 
+        //verify if the web element is displayed or not
+        if (confirmationMessage.isDisplayed()){
+            System.out.println("Confirmation message is displayed. Verification PASSED!");
+        }else{
+            System.out.println("Confirmation message is NOT displayed. Verification FAILED!!!");
+        }
 
 
     }
---
+++ src/test/java/com/cybertek/untilities/WebDriverFactory.java
@@ -0,0 +1,23 @@
+package com.cybertek.untilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    public static WebDriver getDriver(String browserType){
+        if(browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        }else if(browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver();
+            return new FirefoxDriver();
+        }else {
+            System.out.println("Given browser does not exist. Driver =null");
+            return null;
+        }
+
+
+    }
+}
