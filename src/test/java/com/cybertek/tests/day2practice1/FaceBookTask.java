d70913881996e6e660c6119b6021b821b5c8f498
minara35 <minara35@gmail.com>
2020-09-06 11:38:53 -0400

    add day3

---
+++ src/test/java/com/cybertek/tests/day2practice1/FaceBookTask.java
@@ -0,0 +1,33 @@
+package com.cybertek.tests.day2practice1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FaceBookTask {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        String expectedTitle="Log into Facebook | Facebook";
+       driver.findElement(By.id("email")).sendKeys("minara35@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("M061717@m");
+        driver.findElement(By.name("login")).click();
+    String actualTitle = driver.getTitle();
+
+       if(actualTitle.equals(expectedTitle)){
+            System.out.println("Header Verification PASSED!!");
+        }else{
+            System.out.println("Header Verification FAILED!!");
+        }
+        driver.quit();
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/cybertek/tests/day3_cssSelector_xpath/WT_CybertekLinkVerification.java
@@ -0,0 +1,40 @@
+package com.cybertek.tests.day3_cssSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+TC #3: PracticeCybertek/ForgotPassword URL verification
+1.Open Chrome browser
+2.Go to http://practice.cybertekschool.com/forgot_password
+3.Enter any email into input box
+4.Click on Retrieve password
+5.Verify URL contains: Expected: “email_sent”
+6.Verify textbox displayed the content as expected.Expected: “Your e-mail’s been sent!”
+ */
+public class WT_CybertekLinkVerification {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //2.Go to http://practice.cybertekschool.com/forgot_password
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+
+        //3.Enter any email into input box
+        driver.findElement(By.name("email")).sendKeys("minara@domain.com");
+       // 4.Click on Retrieve password
+        driver.findElement(By.id("form_submit")).click();
+
+        String expectedInUrl = "email_sent";
+        String actualUrl = driver.getCurrentUrl();
+        if(actualUrl.contains(expectedInUrl)){
+            System.out.println("URL verification PASSED");
+        }else {
+            System.out.println("URL verification FAILED");
+        }
+
+
+    }
+}
