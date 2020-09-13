60f987b2ab928eccbf9c7013892f2acaae6982c9
minara35 <minara35@gmail.com>
2020-09-13 15:36:17 -0400

    day5

--- src/test/java/com/cybertek/tests/day5_testNG_intro/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro/TestNG_Introduction.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day5_testNG_intro;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 
@@ -40,4 +41,18 @@ public class TestNG_Introduction {
     }
 
 
+@Test
+    public void test3(){
+
+    String str1 = "TestNG";
+    String str2 = "TestNG";
+    String str3 = "afkjkh";
+
+    Assert.assertTrue(str1.equals(str2));
+    Assert.assertTrue(str1.equals(str3), "Str1 is not equal to Str3");
+    System.out.println("JUst checking if this line will run????? ");
+
+    Assert.assertEquals(str1, str2, "Failure message just in case str1 is not equals to str2");
     }
+
+}
