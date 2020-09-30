c71753cc906fa851db12a78117a9362da59c7428
minara35 <minara35@gmail.com>
2020-09-30 14:42:28 -0400

    add selfpractice

--- src/test/java/com/cybertek/selfPractice/Bitrix2.java
+++ src/test/java/com/cybertek/selfPractice/Bitrix2.java
@@ -2,13 +2,16 @@ package com.cybertek.selfPractice;
 
 import com.cybertek.untilities.BrowserUtils;
 import com.cybertek.untilities.WebDriverFactory;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Bitrix {
+public class Bitrix2 {
     WebDriver driver;
 
     @BeforeMethod
@@ -19,8 +22,6 @@ public class Bitrix {
         driver.findElement(By.xpath("//input[@type='text']")).sendKeys("helpdesk15@cybertekschool.com");
         driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser" + Keys.ENTER);
         BrowserUtils.wait(3);
-
-
         driver.findElement(By.xpath("//span[@class='feed-add-post-form-link feed-add-post-form-link-active']")).click();
     }
 /*
@@ -48,59 +49,19 @@ public class Bitrix {
         //  wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("bx-editor-iframe"));
 
 //Now find the element Record video
-        WebElement warningAlertButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[@class='feed-add-post-form-but-cnt feed-add-videomessage'])[1]")));
-        warningAlertButton.click();
-
-       driver.findElement(By.xpath("//span[@class='popup-window-button popup-window-button-blue']")).click();
-      // BrowserUtils.wait(6);
-
-      // Alert alert = driver.switchTo().alert();
-
-      // alert.accept();
-
-      //  BrowserUtils.wait(2);
+      //  WebElement warningAlertButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[@class='feed-add-post-form-but-cnt feed-add-videomessage'])[1]")));
+       // warningAlertButton.click();
+////span[@class='bxhtmled-top-bar-btn bxhtmled-button-video']
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@title='Insert video']"))).click();
 
 
-        //driver.findElement(By.xpath("//span[@class='popup-window-button popup-window-button-blue']")).click();
+        BrowserUtils.wait(6);
 
-     // driver.switchTo().alert().accept();
+       WebElement el = driver.findElement(By.xpath("//input[@id='video_idPostFormLHE_blogPostForm-source']"));
+       el.sendKeys("https://www.youtube.com/watch?v=b4qMka1WJIs");
 
+       driver.findElement(By.xpath("//input[@value='Save']")).click();
 
-      // Switching to Alert
-      //  Alert alert = driver.switchTo().alert();
-
-        // Capturing alert message.
-
-
-        //String alertMessage= driver.switchTo().alert().getText();
-
-        // Displaying alert message
-        //System.out.println(alertMessage);
-      // BrowserUtils.wait(5);
-
-        // Accepting alert
-     //   alert.accept();
-
-
-//Once all your stuff done with this frame need to switch back to default
-        // driver.switchTo().defaultContent();
-
-
-        // WebElement warningAlertButton = driver.findElement(By.xpath("(//span[contains(@class,'feed-add-post-form-but-cnt feed-add-videomessage') and contains(text(), 'Record Video')])[1]"));
-
-
-        //1- Create Alert instance and switch to alert
-       // driver.switchTo().alert();
-       // BrowserUtils.wait(2);
-
-        //2- Use "alert" instance to accept the javascript alert(popup)
-
-
-        //Locating the result text web element
-        // WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
-
-        //assert "resultText" is displayed
-        // Assert.assertTrue(resultText.isDisplayed(), "Result text is not displayed. Verification failed!!!");
 
 
     }
